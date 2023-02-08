89e56e8d229284a79b4aa0111a7bd524b629d5be
blanderbit <yarik.roterdamskiy@gmail.com>
2023-02-08 22:20:33 +0200

    add rollup analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */ 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import visualizer from "rollup-plugin-visualizer";
 import path from 'path'
 
 // https://vitejs.dev/config/
@@ -20,7 +21,14 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      open: process.env.VITE_APP_RUN_BUNDLE_ANALYZER,
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
